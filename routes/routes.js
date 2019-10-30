4df5351f8a6bc748a379bf960ff776750af2cb16
Sunny Sultan <sunnysultan1640@gmail.com>
2019-10-30 18:35:15 +0600

    Perfect BoilerPlate setUp with Test file and Test DB

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,8 @@
 const DriversController = require('../controllers/drivers_controller');
 
 module.exports = (app) => {
-  // Run this function whenever someone goes to
-  // localhost:3050/
-  app.get('/', DriversController.index);
+  app.post('/api/drivers', DriversController.create);
+  app.put('/api/drivers/:id', DriversController.edit);
+  app.delete('/api/drivers/:id', DriversController.delete);
+  app.get('/api/drivers', DriversController.index);
 };
