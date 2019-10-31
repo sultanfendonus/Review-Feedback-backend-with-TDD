b549913a6218dc4402c38255c16e2e6695cf3562
Sunny Sultan <sunnysultan1640@gmail.com>
2019-10-31 19:15:31 +0600

    Password hash complete by bcrypt

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,11 +1,19 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports = {
   create(req, res, next) {
     const userProp = req.body;
-    User.create(userProp)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        
+      userProp.password = hash
+      User.create(userProp)
         .then(user => res.send(user))
         .catch(next)
+        
+    });
+    
   }
 
 };
